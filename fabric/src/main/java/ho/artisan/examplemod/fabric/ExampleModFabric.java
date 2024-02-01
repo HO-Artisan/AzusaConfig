91b2e59b92f23ac080deb711f2aafcb3e41c35c9
TexTrue <65154269+TexBlock@users.noreply.github.com>
2024-02-01 11:29:31 +0800

    Initial commit

---
+++ fabric/src/main/java/ho/artisan/examplemod/fabric/ExampleModFabric.java
@@ -0,0 +1,11 @@
+package ho.artisan.examplemod.fabric;
+
+import ho.artisan.examplemod.ExampleMod;
+import net.fabricmc.api.ModInitializer;
+
+public class ExampleModFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        ExampleMod.init();
+    }
+}
---
+++ neoforge/src/main/java/ho/artisan/examplemod/neoforge/ExampleModNeoForge.java
@@ -0,0 +1,11 @@
+package ho.artisan.examplemod.neoforge;
+
+import ho.artisan.examplemod.ExampleMod;
+import net.neoforged.fml.common.Mod;
+
+@Mod(ExampleMod.MOD_ID)
+public class ExampleModNeoForge {
+    public ExampleModNeoForge() {
+        ExampleMod.init();
+    }
+}
