2b06233089775d284ec5278a22de7d4863fc79e2
TexTrue <65154269+TexBlock@users.noreply.github.com>
2024-02-03 22:08:17 +0800

    clean

--- forge/src/main/java/ho/artisan/azusaconfig/forge/AzusaConfigModForge.java
+++ forge/src/main/java/ho/artisan/azusaconfig/forge/AzusaConfigModForge.java
@@ -2,12 +2,15 @@ package ho.artisan.azusaconfig.forge;
 
 import ho.artisan.azusaconfig.AzusaConfigMod;
 import ho.artisan.azusaconfig.forge.impl.entrypoint.EntrypointStorage;
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-
+import net.minecraftforge.network.NetworkConstants;
 
 @Mod(AzusaConfigMod.MOD_ID)
 public class AzusaConfigModForge {
     public AzusaConfigModForge() {
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
         EntrypointStorage.loadAll();
         AzusaConfigMod.init();
     }
