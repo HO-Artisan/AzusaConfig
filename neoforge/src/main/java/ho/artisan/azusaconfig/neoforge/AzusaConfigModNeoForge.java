d19ce1b85a8ee3e4e9e751edb3da6de82f330ea0
TexTrue <65154269+TexBlock@users.noreply.github.com>
2024-02-23 22:17:04 +0800

    fix

--- neoforge/src/main/java/ho/artisan/azusaconfig/neoforge/AzusaConfigModNeoForge.java
+++ neoforge/src/main/java/ho/artisan/azusaconfig/neoforge/AzusaConfigModNeoForge.java
@@ -2,11 +2,15 @@ package ho.artisan.azusaconfig.neoforge;
 
 import ho.artisan.azusaconfig.AzusaConfigMod;
 import ho.artisan.azusaconfig.neoforge.impl.entrypoint.EntrypointStorage;
+import net.neoforged.fml.IExtensionPoint;
+import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.network.NetworkConstants;
 
 @Mod(AzusaConfigMod.MOD_ID)
 public class AzusaConfigModNeoForge {
     public AzusaConfigModNeoForge() {
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
         EntrypointStorage.loadAll();
         AzusaConfigMod.init();
     }
