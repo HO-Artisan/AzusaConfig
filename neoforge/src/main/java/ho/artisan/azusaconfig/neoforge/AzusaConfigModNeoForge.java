12f72852f380965d58329baf7b67d434e48576be
TexTrue <3140846162@qq.com>
2024-08-18 14:08:56 +0800

    clean

--- neoforge/src/main/java/ho/artisan/azusaconfig/neoforge/AzusaConfigModNeoForge.java
+++ neoforge/src/main/java/ho/artisan/azusaconfig/neoforge/AzusaConfigModNeoForge.java
@@ -1,16 +1,12 @@
 package ho.artisan.azusaconfig.neoforge;
 
 import ho.artisan.azusaconfig.AzusaConfigMod;
-import ho.artisan.azusaconfig.impl.config.AzusaConfigImpl;
-import net.neoforged.fml.IExtensionPoint;
-import net.neoforged.fml.ModLoadingContext;
+import ho.artisan.azusaconfig.impl.AzusaConfigImpl;
 import net.neoforged.fml.common.Mod;
 
 @Mod(AzusaConfigMod.MOD_ID)
 public class AzusaConfigModNeoForge {
-
     public AzusaConfigModNeoForge() {
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> IExtensionPoint.DisplayTest.IGNORESERVERONLY, (a, b) -> true));
         AzusaConfigImpl.init();
     }
 }
